7d0677519fe2057af554c2bc825106a1c3c35e99
mdriaz <riazmd582@gmail.com>
2022-08-12 20:17:20 +0600

    need to work on flutter slidable package functionality

--- lib/screens/phonebook/contacts_tab.dart
+++ lib/screens/phonebook/contacts_tab.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:sms_net_bd/services/contacts.dart';
 import 'package:sms_net_bd/utils/constants.dart';
 
@@ -81,18 +82,71 @@ class _ContactsTabState extends State<ContactsTab> {
         itemBuilder: (BuildContext context, int index) {
           if (index < items.length) {
             final item = items[index];
-            return ListTile(
-              title: item['name'].toString().isNotEmpty
-                  ? Text(item['name'])
-                  : const Text(
-                      'Unknown',
-                      style: TextStyle(
-                        fontStyle: FontStyle.italic,
-                        color: Colors.grey,
+            return Slidable(
+              key: const ValueKey(0),
+              // The start action pane is the one at the left or the top side.
+              startActionPane: ActionPane(
+                // A motion is a widget used to control how the pane animates.
+                motion: const ScrollMotion(),
+
+                // A pane can dismiss the Slidable.
+                dismissible: DismissiblePane(onDismissed: () {}),
+
+                // All actions are defined in the children parameter.
+                children: [
+                  // A SlidableAction can have an icon and/or a label.
+                  SlidableAction(
+                    onPressed: null,
+                    backgroundColor: Color(0xFFFE4A49),
+                    foregroundColor: Colors.white,
+                    icon: Icons.delete,
+                    label: 'Delete',
+                  ),
+                  SlidableAction(
+                    onPressed: null,
+                    backgroundColor: Color(0xFF21B7CA),
+                    foregroundColor: Colors.white,
+                    icon: Icons.share,
+                    label: 'Share',
+                  ),
+                ],
+              ),
+
+              // The end action pane is the one at the right or the bottom side.
+              endActionPane: const ActionPane(
+                motion: ScrollMotion(),
+                children: [
+                  SlidableAction(
+                    // An action can be bigger than the others.
+                    flex: 2,
+                    onPressed: null,
+                    backgroundColor: Color(0xFF7BC043),
+                    foregroundColor: Colors.white,
+                    icon: Icons.archive,
+                    label: 'Archive',
+                  ),
+                  SlidableAction(
+                    onPressed: null,
+                    backgroundColor: Color(0xFF0392CF),
+                    foregroundColor: Colors.white,
+                    icon: Icons.save,
+                    label: 'Save',
+                  ),
+                ],
+              ),
+              child: ListTile(
+                title: item['name'].toString().isNotEmpty
+                    ? Text(item['name'])
+                    : const Text(
+                        'Unknown',
+                        style: TextStyle(
+                          fontStyle: FontStyle.italic,
+                          color: Colors.grey,
+                        ),
                       ),
-                    ),
-              subtitle: Text(item['email'] ?? ''),
-              trailing: Text(item['number'].toString()),
+                subtitle: Text(item['email'] ?? ''),
+                trailing: Text(item['number'].toString()),
+              ),
             );
           }
 
