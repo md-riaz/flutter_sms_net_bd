245bd31c6f7d8b6f3da2f4202ada4d532d07e180
mdriaz <riazmd582@gmail.com>
2022-08-16 09:02:45 +0600

    need to work on templates page

--- lib/screens/messaging/templates_tab.dart
+++ lib/screens/messaging/templates_tab.dart
@@ -3,6 +3,7 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:sms_net_bd/services/templates.dart';
+import 'package:sms_net_bd/utils/constants.dart';
 import 'package:sms_net_bd/widgets/confirmation.dart';
 
 class TemplateTab extends StatefulWidget {
@@ -14,6 +15,7 @@ class TemplateTab extends StatefulWidget {
 
 class _TemplateTabState extends State<TemplateTab> {
   List<Map> templates = [];
+  bool isLoading = false;
 
   @override
   void initState() {
@@ -43,12 +45,16 @@ class _TemplateTabState extends State<TemplateTab> {
     );
 
     if (confirm == true) {
+      setState(() {
+        isLoading = true;
+      });
       if (!mounted) return;
       final resp = await deleteTemplate(context, mounted, delId);
 
       if (resp == true) {
         setState(() {
           templates.removeWhere((item) => item['id'] == delId);
+          isLoading = false;
         });
       }
     }
@@ -56,85 +62,87 @@ class _TemplateTabState extends State<TemplateTab> {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          ListView.separated(
-            key: const ValueKey(0),
-            shrinkWrap: true,
-            physics: const NeverScrollableScrollPhysics(),
-            itemCount: templates.length,
-            itemBuilder: (BuildContext context, int i) {
-              final Map item = templates[i];
-              log(templates.toString());
-              return ExpansionTile(
-                title: Text(item['name']),
-                children: <Widget>[
-                  Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(
-                            right: 16, left: 16, bottom: 16),
-                        child: Text(item['text']),
-                      ),
-                      const Divider(
-                        height: 1,
-                      ),
-                      IntrinsicHeight(
-                        child: ButtonBar(
-                          alignment: MainAxisAlignment.spaceEvenly,
-                          mainAxisSize: MainAxisSize.max,
-                          buttonPadding: const EdgeInsets.all(0),
+    return isLoading
+        ? preloader
+        : SingleChildScrollView(
+            child: Column(
+              children: [
+                ListView.separated(
+                  key: const ValueKey(0),
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: templates.length,
+                  itemBuilder: (BuildContext context, int i) {
+                    final Map item = templates[i];
+                    log(templates.toString());
+                    return ExpansionTile(
+                      title: Text(item['name']),
+                      children: <Widget>[
+                        Column(
                           children: [
-                            TextButton.icon(
-                              onPressed: () {},
-                              icon: const Icon(Icons.edit),
-                              label: const Text(
-                                'Edit',
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.normal,
-                                ),
-                              ),
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  right: 16, left: 16, bottom: 16),
+                              child: Text(item['text']),
                             ),
-                            const VerticalDivider(
-                              thickness: 0.5,
+                            const Divider(
+                              height: 1,
                             ),
-                            TextButton.icon(
-                              onPressed: () {
-                                handleDelete(item);
-                              },
-                              icon: const Icon(
-                                Icons.delete,
-                                color: Colors.red,
+                            IntrinsicHeight(
+                              child: ButtonBar(
+                                alignment: MainAxisAlignment.spaceEvenly,
+                                mainAxisSize: MainAxisSize.max,
+                                buttonPadding: const EdgeInsets.all(0),
+                                children: [
+                                  TextButton.icon(
+                                    onPressed: () {},
+                                    icon: const Icon(Icons.edit),
+                                    label: const Text(
+                                      'Edit',
+                                      style: TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.normal,
+                                      ),
+                                    ),
+                                  ),
+                                  const VerticalDivider(
+                                    thickness: 0.5,
+                                  ),
+                                  TextButton.icon(
+                                    onPressed: () {
+                                      handleDelete(item);
+                                    },
+                                    icon: const Icon(
+                                      Icons.delete,
+                                      color: Colors.red,
+                                    ),
+                                    label: const Text(
+                                      'Delete',
+                                      style: TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.normal,
+                                        color: Colors.red,
+                                      ),
+                                    ),
+                                  ),
+                                ],
                               ),
-                              label: const Text(
-                                'Delete',
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.normal,
-                                  color: Colors.red,
-                                ),
-                              ),
-                            ),
+                            )
                           ],
                         ),
-                      )
-                    ],
-                  ),
-                  // add more data that you want like this
-                ],
-              );
-            },
-            separatorBuilder: (BuildContext context, int index) {
-              return const Divider(
-                thickness: 1,
-                height: 1,
-              );
-            },
-          ),
-        ],
-      ),
-    );
+                        // add more data that you want like this
+                      ],
+                    );
+                  },
+                  separatorBuilder: (BuildContext context, int index) {
+                    return const Divider(
+                      thickness: 1,
+                      height: 1,
+                    );
+                  },
+                ),
+              ],
+            ),
+          );
   }
 }
