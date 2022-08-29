75707bd44419714eef213ecb2ee738cb50da575b
mdriaz <riazmd582@gmail.com>
2022-08-29 20:29:38 +0600

    monitor page refresh in every 5 sec done

--- lib/screens/monitor.dart
+++ lib/screens/monitor.dart
@@ -1,6 +1,9 @@
+import 'dart:async';
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:liquid_progress_indicator/liquid_progress_indicator.dart';
 import 'package:sms_net_bd/services/monitor.dart';
 import 'package:sms_net_bd/utils/constants.dart';
 import 'package:sms_net_bd/widgets/app_bar.dart';
@@ -15,22 +18,19 @@ class MonitorScreen extends StatefulWidget {
 
 class _MonitorScreenState extends State<MonitorScreen> {
   Future? pageFuture;
-  bool isLoading = true;
+  late Timer timer;
 
   @override
   void initState() {
     super.initState();
     pageFuture = getPageData();
+    timer = Timer.periodic(const Duration(seconds: 5), (Timer t) {
+      setState(() {});
+    });
   }
 
   Future getPageData() async {
     final data = await getMonitorData(context, mounted);
-
-    if (mounted) {
-      setState(() {
-        isLoading = false;
-      });
-    }
 
     return data;
   }
@@ -61,12 +61,18 @@ class _MonitorScreenState extends State<MonitorScreen> {
                 return Text('🥺 $error');
               } else if (snapshot.hasData) {
                 final Map data = snapshot.data as Map;
-                log(data.toString());
+                log('calling 5 sec');
                 return SingleChildScrollView(
                   child: Container(
                     margin: const EdgeInsets.all(16),
                     child: Column(
                       children: [
+                        Resources(
+                          CPU: data['monitor']['cpu'],
+                          RAM: data['monitor']['ram'],
+                          Disk: data['monitor']['disk'],
+                        ),
+                        formSpacer,
                         Summery(monitorData: data['monitor']),
                       ],
                     ),
@@ -82,15 +88,19 @@ class _MonitorScreenState extends State<MonitorScreen> {
   }
 }
 
-class Resources extends StatefulWidget {
-  final Map data;
-  const Resources({Key? key, required this.data}) : super(key: key);
+class Resources extends StatelessWidget {
+  final Map CPU;
+  final Map Disk;
+  final Map RAM;
+
+  const Resources({
+    Key? key,
+    required this.CPU,
+    required this.Disk,
+    required this.RAM,
+  }) : super(key: key);
 
   @override
-  State<Resources> createState() => _ResourcesState();
-}
-
-class _ResourcesState extends State<Resources> {
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -104,6 +114,178 @@ class _ResourcesState extends State<Resources> {
           ),
         ),
         formSpacer,
+        Wrap(
+          runSpacing: 12,
+          alignment: WrapAlignment.center,
+          runAlignment: WrapAlignment.center,
+          children: [
+            Container(
+              width: double.infinity,
+              padding: const EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Colors.cyan.shade50,
+                borderRadius: BorderRadius.circular(2),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'CPU',
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),
+                      ),
+                      formSpacer,
+                      Text(
+                        'CPU Core: ${CPU['core']}',
+                        style: TextStyle(
+                          color: Colors.cyan.shade400,
+                        ),
+                      ),
+                      const SizedBox(height: 2),
+                      Text(
+                        'Free: ${CPU['free']}%',
+                        style: TextStyle(
+                          color: Colors.cyan.shade400,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 90,
+                    width: 90,
+                    child: LiquidCircularProgressIndicator(
+                      value: CPU['usage'] / 100, // Defaults to 0.5.
+                      valueColor: AlwaysStoppedAnimation(
+                        Colors.cyan.shade200,
+                      ), // Defaults to the current Theme's accentColor.
+                      backgroundColor: Colors
+                          .white, // Defaults to the current Theme's backgroundColor.
+                      borderColor: Colors.cyan.shade300,
+                      borderWidth: 3.0,
+                      direction: Axis.vertical,
+                      // The direction the liquid moves (Axis.vertical = bottom to top, Axis.horizontal = left to right). Defaults to Axis.vertical.
+                      center: Text("${CPU['usage']}%"), //text inside it
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              padding: const EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Colors.pink.shade50,
+                borderRadius: BorderRadius.circular(2),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'RAM',
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),
+                      ),
+                      formSpacer,
+                      Text(
+                        'Total: ${RAM['total']}',
+                        style: TextStyle(
+                          color: Colors.pink.shade400,
+                        ),
+                      ),
+                      const SizedBox(height: 2),
+                      Text(
+                        'Free: ${RAM['free']}',
+                        style: TextStyle(
+                          color: Colors.pink.shade400,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 90,
+                    width: 90,
+                    child: LiquidCircularProgressIndicator(
+                      value: RAM['usage'] / 100, // Defaults to 0.5.
+                      valueColor: AlwaysStoppedAnimation(
+                        Colors.pink.shade100,
+                      ), // Defaults to the current Theme's accentColor.
+                      backgroundColor: Colors
+                          .white, // Defaults to the current Theme's backgroundColor.
+                      borderColor: Colors.pink.shade300,
+                      borderWidth: 3.0,
+                      direction: Axis.vertical,
+                      // The direction the liquid moves (Axis.vertical = bottom to top, Axis.horizontal = left to right). Defaults to Axis.vertical.
+                      center: Text("${RAM['used']}"), //text inside it
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              padding: const EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Colors.teal.shade50,
+                borderRadius: BorderRadius.circular(2),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'DISK',
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),
+                      ),
+                      formSpacer,
+                      Text(
+                        'Total: ${Disk['total']}',
+                        style: TextStyle(
+                          color: Colors.teal.shade400,
+                        ),
+                      ),
+                      const SizedBox(height: 2),
+                      Text(
+                        'Free: ${Disk['free']}',
+                        style: TextStyle(
+                          color: Colors.teal.shade400,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 90,
+                    width: 90,
+                    child: LiquidCircularProgressIndicator(
+                      value: Disk['usage'] / 100, // Defaults to 0.5.
+                      valueColor: AlwaysStoppedAnimation(
+                        Colors.teal.shade200,
+                      ), // Defaults to the current Theme's accentColor.
+                      backgroundColor: Colors
+                          .white, // Defaults to the current Theme's backgroundColor.
+                      borderColor: Colors.teal.shade300,
+                      borderWidth: 3.0,
+                      direction: Axis.vertical,
+                      // The direction the liquid moves (Axis.vertical = bottom to top, Axis.horizontal = left to right). Defaults to Axis.vertical.
+                      center: Text("${Disk['used']}"), //text inside it
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ],
     );
   }
@@ -112,7 +294,9 @@ class _ResourcesState extends State<Resources> {
 class Summery extends StatelessWidget {
   final Map monitorData;
 
-  const Summery({Key? key, required this.monitorData}) : super(key: key);
+  Summery({Key? key, required this.monitorData}) : super(key: key);
+
+  final NumberFormat formatter = NumberFormat.decimalPattern('en_us');
 
   @override
   Widget build(BuildContext context) {
@@ -152,7 +336,7 @@ class Summery extends StatelessWidget {
                   formSpacer,
                   formSpacer,
                   Text(
-                    monitorData['sms_requests'].toString(),
+                    formatter.format(monitorData['sms_requests']),
                     style: TextStyle(
                       fontSize: 28,
                       color: Colors.blue.shade400,
@@ -180,7 +364,7 @@ class Summery extends StatelessWidget {
                   formSpacer,
                   formSpacer,
                   Text(
-                    monitorData['pending'].toString(),
+                    formatter.format(monitorData['pending']),
                     style: TextStyle(
                       fontSize: 28,
                       color: Colors.purple.shade400,
@@ -209,7 +393,7 @@ class Summery extends StatelessWidget {
                   ),
                   formSpacer,
                   Text(
-                    monitorData['sent'].toString(),
+                    formatter.format(monitorData['sent']),
                     style: TextStyle(
                       fontSize: 28,
                       color: Colors.green.shade400,
@@ -237,7 +421,7 @@ class Summery extends StatelessWidget {
                   formSpacer,
                   formSpacer,
                   Text(
-                    monitorData['failed'].toString(),
+                    formatter.format(monitorData['failed']),
                     style: TextStyle(
                       fontSize: 28,
                       color: Colors.red.shade400,
@@ -265,7 +449,7 @@ class Summery extends StatelessWidget {
                   formSpacer,
                   formSpacer,
                   Text(
-                    monitorData['errors'].toString(),
+                    formatter.format(monitorData['errors']),
                     style: TextStyle(
                       fontSize: 28,
                       color: Colors.orange.shade400,
